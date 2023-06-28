79eca76f4257cde3a8f3b11fc43b20c3165b3934
rahul04052000 <90526719+rahul04052000@users.noreply.github.com>
2023-06-29 01:39:13 +0530

    body

---
+++ src/body/Body.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./body.css"
+export default function Body() {
+    return (
+        <div className='body-parent'>
+            <div className='body_child'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit
+            </div>
+            <div className='body_child'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit
+            </div>
+            <div className='body_child'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit
+            </div>
+            <div className='body_child'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit
+            </div>
+
+        </div>
+    )
+}
