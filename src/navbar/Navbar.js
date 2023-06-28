1b1baa1cd25ac71e84ff23037696dee2c5f29ddc
rahul04052000 <90526719+rahul04052000@users.noreply.github.com>
2023-06-29 01:07:18 +0530

    initial
    initial

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,18 @@
+import "./navbar.css"
+import React from 'react'
+
+export default function navbar() {
+    return(
+        <div className="heading">
+            <div className="main">
+                HACKFEST-23
+            </div>
+            <div className="sub">
+                By AXISCADES-MISTRAL
+            </div>
+
+            
+
+        </div>
+    )
+}
